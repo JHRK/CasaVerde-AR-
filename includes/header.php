2dc2bcc299ab88792f135ad801072054429ecb38
JHRK <jhrk05@gmail.com>
2024-02-26 17:31:33 -0300

    s

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="icon" type="image/png" href="./Union.png">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&family=DM+Sans:opsz,wght@9..40,600&family=Gabarito:wght@600&family=Manrope:wght@700&family=Open+Sans&family=Poppins:wght@600&family=Sono&family=Space+Mono:wght@700&family=Trispace:wght@800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@700&family=Questrial&display=swap" rel="stylesheet">
+    <title>CASA VERDE /AR/</title>
+    <link rel="icon" type="image/x-icon" href="./Img_Strg/Union.png">
+  </head>
+    <body>
+      
+      <header >
+asd
+      </header>
+    
